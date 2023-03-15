9a6009a517e388b62a36a9018e495dd382313382
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-03-15 19:41:40 +0000

    Refactored code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom/client';
 function Greeting() {
   return (
     <div>
-      <h2>First Component</h2>
+      <h2>Hello World</h2>
     </div>
   );
 }
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h2', {}, 'hello world')
+//   )
+// }
 
 // function Greeting() {
 //   return React.createElement('h2', {}, 'hello world')
