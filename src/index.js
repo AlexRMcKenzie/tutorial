ad94bff1689272c3fb70f67cdf8b69574dc4ae91
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-03-15 19:31:05 +0000

    Added hello world Greeting component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
-  return <h1>First Component</h1>
-}
+// function Greeting() {
+//   return <h1>First Component</h1>
+// }
+
+// function Greeting() {
+//   return React.createElement('h2', {}, 'hello world')
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
