dea51c0f376291a4faf1aecec7fc3a64c60372b1
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-03-16 18:52:40 +0000

    Refactored inlineHeadingStyles

--- src/index.js
+++ src/index.js
@@ -26,7 +26,12 @@ const Book = () => {
 const Image = () => <img src="./images/book-1.jpg" alt="Bored of Lunch: The Healthy Air Fryer Book" />
 const Title = () => <h2>Bored of Lunch: The Healthy Air Fryer Book</h2>
 const Author = () => {
-  return <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>Nathan Anthony</h4>
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  }
+  return <h4 style={inlineHeadingStyles}>Nathan Anthony</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
