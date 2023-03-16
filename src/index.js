28a4d574ed1f97b18612aff6233ec54bced82fca
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-03-16 18:51:16 +0000

    Added inline styling to author component

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const Book = () => {
 const Image = () => <img src="./images/book-1.jpg" alt="Bored of Lunch: The Healthy Air Fryer Book" />
 const Title = () => <h2>Bored of Lunch: The Healthy Air Fryer Book</h2>
 const Author = () => {
-  return <h4>Nathan Anthony</h4>
+  return <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>Nathan Anthony</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
