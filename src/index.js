de57502c0a750477043fb4e190fd13cc7edc2100
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-03-16 19:11:39 +0000

    Refactored code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const author = "Nathan Anthony";
+const title = "Bored of Lunch: The Healthy Air Fryer Book";
+const img = "./images/book-1.jpg"
 const Booklist = () => {
   return (
     <section className='booklist'>
@@ -13,14 +16,13 @@ const Booklist = () => {
   )
 }
 
-const author = "Nathan Anthony";
+
 const Book = () => {
-  const title = "Bored of Lunch: The Healthy Air Fryer Boooooooook";
   return (
     <article className='book'>
-      <img src="./images/book-1.jpg" alt="Bored of Lunch: The Healthy Air Fryer Book" />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
     </article>
   )
 }
